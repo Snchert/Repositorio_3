d8da47ac6df986ac8406eb2adb135c31791e1790
Andres_Sanchez <andysanlink@gmail.com>
2025-04-29 14:30:21 -0600

    .\numeros_romanos.py

---
+++ numeros_romanos.py
@@ -0,0 +1,15 @@
+def primer_digito():
+
+    return 1+1
+
+Numero_ingresado = input('Ingresa el numero romano: ') 
+lista = [Numero_ingresado]
+letras_tupla =tuple(lista)
+conta =1
+while(conta < len(Numero_ingresado)):
+    
+    if():
+        p= 1
+
+
+        
